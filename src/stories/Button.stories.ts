0a0b6109a3e153e89d0042382c50fad62d4ab216
Jono Kolnik <1164060+JonathanKolnik@users.noreply.github.com>
2023-08-21 13:20:00 -0400

    add some modes to main

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -23,20 +23,20 @@ export const Primary: Story = {
   },
   parameters: {
     chromatic: {
-    viewports: [320, 640],
+    // viewports: [320, 640],
 
-      // modes: {
-      //   '320px': {
-      //     viewport: 320
-      //   },
-      //   // 'mobile': {
-      //   //   viewport: 666,
-      //   //   lang: 'es',
-      //   // },
-      //   // 'dark': {
-      //   //   viewport: 666,
-      //   // }
-      // }
+      modes: {
+        '320px': {
+          viewport: 320
+        },
+        'mobile': {
+          viewport: 666,
+          lang: 'es',
+        },
+        'dark': {
+          viewport: 666,
+        }
+      }
     }
   }
 };
